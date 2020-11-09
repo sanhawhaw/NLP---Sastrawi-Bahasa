d41556a0906fd8de06272d5396cc59ec78affe19
sanhawhaw <36259912+sanhawhaw@users.noreply.github.com>
2020-11-09 11:03:16 +0700

    Update Sastrawi_Stemmer_StemmerFactory.py

--- Sastrawi_Stemmer_StemmerFactory.py
+++ Sastrawi_Stemmer_StemmerFactory.py
@@ -1,7 +1,8 @@
 # Python Sastrawi 
 
 # # import StemmerFactory class
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory# create stemmer
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+# create stemmer
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()# stemming process
 sentence = 'mencoba-coba untuk melakukan pertumbuhan akar dalam menanan jagung adalah hal yang memudahkan kita untuk memanennya'
