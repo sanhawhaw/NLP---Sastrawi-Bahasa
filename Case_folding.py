e4bc6801dbb67b4fb085929269a262c054545552
sanhawhaw <36259912+sanhawhaw@users.noreply.github.com>
2020-11-09 11:45:26 +0700

    Create Case_folding.py

---
+++ Case_folding.py
@@ -0,0 +1,8 @@
+# Case folding
+# Tujuan dari case folding untuk mengubah semua huruf dalam dokumen menjadi huruf kecil
+
+kalimat = "Nicholas Saputra, adalah si Mas Ganteng yang selama ini aku maksud."
+lower_case = kalimat.lower()
+print(lower_case)
+# # output
+# # nicholas saputra, adalah si mas ganteng yang selama ini aku maksud.
