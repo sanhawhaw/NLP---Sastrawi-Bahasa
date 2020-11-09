f020c9cd74b5a81ad7a2c31e64a0164f409ce73a
sanhawhaw <36259912+sanhawhaw@users.noreply.github.com>
2020-11-09 12:40:24 +0700

    Create Tokenizing_Pemisah_kata.py

---
+++ Tokenizing_Pemisah_kata.py
@@ -0,0 +1,7 @@
+# Tokenizing
+
+kalimat = "aku dan kau suka dancow. susu enak nikmat pula"
+pisah = kalimat.split()
+print(pisah)
+#output
+# ['aku', 'dan', 'kau', 'suka', 'dancow.', 'susu', 'enak', 'nikmat', 'pula']
